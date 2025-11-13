34eedcb9b45e8b2ede9d85d28040f9156e34ddb9
Mohamed Ali <workmail1010@gmail.com>
2025-11-13 21:47:14 +0200

    feat: enhance app configuration and user list component with translation support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,42 +2,55 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
-  inject,
+  inject
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
-import { provideHttpClient, withFetch  , withInterceptors} from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
-
-
 import { appInterceptor } from './interceptors/app-http.interceptor';
 
-console.log('app.config.ts loaded'); // Log للتأكد من تحميل الملف
+console.log('app.config.ts loaded');
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    // HttpClient مع Interceptors
     provideHttpClient(
+      withFetch(),
       withInterceptors([appInterceptor])
     ),
+
+    // Angular Global Error Listeners
     provideBrowserGlobalErrorListeners(),
+
+    // Zoneless change detection
     provideZonelessChangeDetection(),
+
+    // Router
     provideRouter(routes),
+
+    // Hydration
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
+
+    // ngx-translate
     provideTranslateService({
-      lang: 'ar',
-      fallbackLang: 'en',
+      lang: 'ar',               // اللغة الافتراضية
+      fallbackLang: 'en',       // لغة احتياطية
+      useDefaultLang: true,     // يفرض استخدام اللغة الافتراضية
       loader: provideTranslateHttpLoader({
-        prefix: '/i18n/',
+        prefix: '/i18n/',       // public/i18n/
         suffix: '.json',
       }),
     }),
+
+    // Animations
     provideAnimationsAsync(),
+
+    // Angular Material Snackbar defaults
     {
       provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
       useValue: {
--- src/app/pages/users/users-list/users-list.ts
+++ src/app/pages/users/users-list/users-list.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core'; 
 @Component({
   selector: 'app-users-list',
   standalone: false,
   templateUrl: './users-list.html',
   styleUrls: ['./users-list.scss'],
 })
-export class UsersList {}
+export class UsersList {
+  constructor( private translate: TranslateService) {
+     console.log(this.translate.instant('dashboard')); 
+  }
+}
